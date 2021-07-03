6658b8776838c5a35e663b677c8f2236b8434e79
Bartholomäus Dedersen <git@selbstapotheose.de>
2021-07-03 12:17:49 +0200

    fixed paths

--- hugin_process.sh
+++ hugin_process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-workDir='/Users/sampleuser/sampledir/'
+workDir='/Users/debar/Desktop/Schmetterling'
 
 alignTool='/Applications/Hugin/tools_mac/align_image_stack'
 enfuseTool='/Applications/Hugin/tools_mac/enfuse'
@@ -9,9 +9,9 @@ cd $workDir
 $alignTool -a align_ -m -v IMG*.tif
 
 
-inputFiles=$(ls ${workDir}align*.tif)
+inputFiles=$(ls ${workDir}/align*.tif)
 
-$enfuseTool -o "${workDir}result.jpeg" --compression=100 --contrast-weight=1.00 \
+$enfuseTool -o "${workDir}/result.jpeg" --compression=100 --contrast-weight=1.00 \
   --exposure-weight=0.00 --saturation-weight=0.00 --contrast-window-size=5 \
   --hard-mask --gray-projector=luminance \
   $inputFiles
